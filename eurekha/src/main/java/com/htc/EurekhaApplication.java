6ecbfd031f9d3f96ceaf3dc61ab61de33d7ec52f
Umamaheshgowd <maheshuma971@gmail.com>
2021-09-07 00:02:48 +0530

    done

---
+++ eurekha/src/main/java/com/htc/EurekhaApplication.java
@@ -0,0 +1,13 @@
+package com.htc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekhaApplication.class, args);
+	}
+
+}
---
+++ eurekha/src/test/java/com/htc/EurekhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
