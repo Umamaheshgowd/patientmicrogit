5f47e3a0658583d625f68bb017767fc7624b6210
Umamaheshgowd <maheshuma971@gmail.com>
2021-09-16 19:32:06 +0530

    updated doctormanagement

--- doctormanagement/src/test/java/com/htc/doctormanagement/DoctormanagementApplicationTests.java
+++ doctormanagement/src/test/java/com/htc/doctormanagement/DoctormanagementApplicationTests.java
@@ -1,13 +1,37 @@
 package com.htc.doctormanagement;
 
+import java.time.LocalDate;
+
+import org.jboss.jandex.Main;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import com.htc.doctormanagement.dao.AppointmentInterface;
+import com.htc.doctormanagement.entity.Appointment;
 
 @SpringBootTest
 class DoctormanagementApplicationTests {
+	@Autowired
+	AppointmentInterface appointmentInterface;
 
-	@Test
-	void contextLoads() {
+	private void Main() {
+		// TODO Auto-generated method stub
+		testAppointment();
+
 	}
+	
+	@Test
+	public  void testAppointment() {
+		Appointment appointment = new Appointment();
+		appointment.setAppointmentId(1L);
+		appointment.setAppointmentDate(LocalDate.now());
+		appointment.setDoctorId(2L);
+		
+		appointmentInterface.save(appointment);
+		
+		assertNotNull(appointmentInterface.findById(1L).get());
+	}
+
 
 }
