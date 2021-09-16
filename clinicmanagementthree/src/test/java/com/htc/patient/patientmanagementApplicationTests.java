8402d0346680c96367cfb283461531af3990f2bc
Umamaheshgowd <maheshuma971@gmail.com>
2021-09-16 19:38:47 +0530

    removed testing method in project

--- clinicmanagementthree/src/test/java/com/htc/patient/patientmanagementApplicationTests.java
+++ clinicmanagementthree/src/test/java/com/htc/patient/patientmanagementApplicationTests.java
@@ -1,11 +1,17 @@
 package com.htc.patient;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import com.htc.patient.entity.Patient;
+import com.htc.patient.persistence.PatientInterface;
 
 @SpringBootTest
-class patientmanagementApplicationTests {
 
+class patientmanagementApplicationTests {
+	
+	
 	@Test
 	void contextLoads() {
 	}
